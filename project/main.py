617dd7162f1a60a7b3a7e13e4aeb25f3ea8ab601
glazko1 <glazko_2001@mail.ru>
2020-01-19 19:03:12 +0300

    Add ability to get points from text file

--- project/main.py
+++ project/main.py
@@ -1,9 +1,15 @@
 import folium
+import pandas
 
 map = folium.Map(location=[38.58, -99.09])
 
 feature_group = folium.FeatureGroup(name="Map")
-feature_group.add_child(folium.Marker(location=[38.7, -98.95], popup="Custom Marker", icon=folium.Icon(color="green")))
+
+df = pandas.read_csv("../data/volcanoes.txt")
+for index, row in df.iterrows():
+    feature_group.add_child(folium.Marker(location=[row["LAT"], row["LON"]],
+                                          popup=row["NAME"],
+                                          icon=folium.Icon(color="green")))
 
 map.add_child(feature_group)
 
